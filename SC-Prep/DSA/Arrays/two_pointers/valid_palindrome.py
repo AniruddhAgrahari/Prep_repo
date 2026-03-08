9b54a7d317f5fcce37d57d3f68fdcf6a520133a3
Aniruddh Agrahari <22BAI71310@cuchd.in>
2026-03-08 11:43:26 +0530

    Add valid palindrome implementation with two pointer solution

--- SC-Prep/DSA/Arrays/two_pointers/valid_palindrome.py
+++ SC-Prep/DSA/Arrays/two_pointers/valid_palindrome.py
@@ -3,21 +3,35 @@ def isPalindrome(s:str) -> bool:
     # Clean the string, store it in new string and then compare new_string with it's reverse
     # time complexity: O(n)
     # space complexity: O(n)
-    # Pattern - string cleaning 
+    # Pattern - string cleaning + two pointer
     
-    new_string = ""
-    for char in s:
+    # new_string = ""
+    # for char in s:
 
-        if char.isalnum():
-            new_string += char.lower()
+    #     if char.isalnum():
+    #         new_string += char.lower()
 
-    if new_string == new_string[::-1]:
-            return True
-    else:
-         return False
+    # if new_string == new_string[::-1]:
+    #         return True
+    # else:
+    #      return False
 
+    left = 0
+    right = len(s) - 1
 
+    while left < right:
+        while left < right and not s[left].isalnum():
+            left += 1
+        while left < right and not s[right].isalnum():
+            right -= 1
 
+        if s[left].lower() == s[right].lower():
+            left += 1
+            right -= 1
+        else:
+            return False
+
+    return True
 
 if __name__ == "__main__":
     assert isPalindrome("racecar") == True
