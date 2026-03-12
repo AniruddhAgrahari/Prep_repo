4e6f3f8d47c68e48753ce795fbbe333704be8899
Aniruddh Agrahari <22BAI71310@cuchd.in>
2026-03-12 08:46:11 +0530

    feat:two sum II revision

---
+++ SC-Prep/DSA/Arrays/two_pointers/two_sum_II_practice.py
@@ -0,0 +1,20 @@
+
+
+def two_sum_II(nums, target):
+
+    left = 0
+    right = len(nums) - 1
+
+    while left < right:
+        s = nums[left] + nums[right]
+        if s == target:
+           return [left+1, right+1]
+        elif s > target:
+            right -= 1
+        elif s < target:
+            left += 1
+
+if __name__ == "__main__":
+    assert two_sum_II([1, 2, 4, 8], 3) == [1, 2]
+    assert two_sum_II([1, 3, 5, 7, 9], 16) == [4, 5]
+    assert two_sum_II([3, 4, 5, 6, 10], 9) == [1, 4]
