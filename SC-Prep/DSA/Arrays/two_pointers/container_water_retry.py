5fbfd1c58a1fd3c3d93f3bad39235e93badb3e0f
Aniruddh Agrahari <22BAI71310@cuchd.in>
2026-03-14 18:39:21 +0530

    feat: container water problem re-attempt

---
+++ SC-Prep/DSA/Arrays/two_pointers/container_water_retry.py
@@ -0,0 +1,25 @@
+#Approach:
+# the left pointer is pointing at 0 and the right at len(array) - 1
+# water contained = width(right - left) * min(array[left], array[right])
+# move the pointer inwards whichever has smaller height
+
+def max_area(height: list[int]) -> int:
+
+    left = 0
+    right = len(height) - 1
+    max_water = 0
+
+    while left < right:
+        water_contained = (right - left) * min(height[left], height[right])
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+        if max_water < water_contained:
+            max_water = water_contained
+
+    return max_water
+
+
+assert max_area([2, 3, 5]) == 4
+assert max_area([4, 6, 8,7]) == 12
