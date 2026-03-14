1481522ae2b7f0ab7e24a979264efbda0e7abcdd
Aniruddh Agrahari <22BAI71310@cuchd.in>
2026-03-14 13:17:26 +0530

    feat:group anagrams using hashmap approach

---
+++ SC-Prep/DSA/Arrays/hashmaps/group_anagram.py
@@ -0,0 +1,11 @@
+def groupAnagrams(strs):
+    d = {}
+
+    for word in strs:
+        key = "".join(sorted(word))
+        if key in d:
+            d[key].append(word)
+        else:
+            d[key] = [word]
+
+    return list(d.values())
