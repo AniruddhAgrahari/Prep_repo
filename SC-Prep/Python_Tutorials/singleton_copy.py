b555484f0bf9f1021871f21abe24ebb919f5ce2f
Aniruddh Agrahari <22BAI71310@cuchd.in>
2026-03-26 16:17:56 +0530

    feat: add design-patterns/singleton/bank-config.py with test cases

--- SC-Prep/Python_Tutorials/singleton_copy.py
+++ SC-Prep/Python_Tutorials/singleton_copy.py
@@ -2,12 +2,21 @@ class BankConfig:
     _instance = None
 
     def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-            cls._instance.interest_rate = 0.04
-            cls._instance.transaction_limit = 100000
-        return cls._instance
+      if cls._instance is None:
+        cls._instance = super().__new__(cls)
+        cls._instance.interest_rate = 0.04
+        cls._instance.transaction_limit = 25000
+      return cls._instance
     
+
+
 config1 = BankConfig()
 config2 = BankConfig()
-print(config1 or config2)
+print(config1 is config2)
+print(config1.interest_rate)
+print(config2.interest_rate)
+
+
+# print(config1 is config2)  # Should print True
+
+
